0d72a7e1e758ef0c7d7ed57d11a6f793dfcdda34
Fkorsrud <126823227+Fkorsrud@users.noreply.github.com>
2023-04-25 15:18:41 -0600

    started planting flowers

--- proposal.md
+++ proposal.md
@@ -13,17 +13,14 @@ A game where you run a flower shop
 - start page
 - plant flowers in a pot
 - the flowers grow
-- "pick" the flowers (maybe more then one per pot, like bunches of them) once they are done and store them
-- put the flowers in pots/vases/jars or whatever
-- sell the flowers
-- have money
+- "pick" the flowers once they are done and store them
+- put the flowers in vases
 - different type of flowers
-- cat to pet
+- saves the game
+- separate "rooms", greenhouse
 - a "store" to buy things
 - have things to buy with the money like seeds  
 - stores seeds - not an infinite amount of seeds
-- saves the game
-- separate "rooms", greenhouse
 - buy more pots to grow more flowers at a time
 - buy fertilizer to make them grow faster
 
@@ -31,7 +28,8 @@ A game where you run a flower shop
 ## Nice to have list
 
 - store area to sell your flowers from
-- decorate the store
+- talk to different customers
 - have atributes for each flower that makes it more/less money depending on what the customer wants
 - book that tells you about them
-- talk to different customers
+- sell the flowers
+- have money
--- sketch.js
+++ sketch.js
@@ -6,11 +6,62 @@
 // - describe what you did to take this project "above and beyond"
 
 
+
+let room = 0;
+
+let flowers = [];
+let seeds = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+
 }
 
 function draw() {
   background(220);
-  rect(mouseX, mouseY, 50,50);
+  draw_pots();
+  
+}
+
+function draw_pots(){
+  rect( 400, 250, 50, 50);
+  rect( 600, 250, 50, 50);
+  rect(800, 250, 50,50);
+  rect( 400, 500, 50, 50);
+  rect( 600, 500, 50, 50);
+  rect(800, 500, 50,50);
+}
+
+function plant_seed(pot, seedtpe){
+  console.log(pot);
+}
+
+function mousePressed(){
+  console.log("uhh");
+  let pot = "";
+  if (mouseX > 375 && mouseX < 425 && mouseY > 225 && mouseY < 275){
+    pot = "one";
+    plant_seed(pot, "red");
+  }
+  else if (mouseX > 575 && mouseX <625 && mouseY > 225 && mouseY < 275){
+    pot = "two";
+    plant_seed(pot, "red");
+  }
+  else if (mouseX > 775 && mouseX < 825 && mouseY > 225 && mouseY < 275){
+    pot = "three";
+    plant_seed(pot, "red");
+  }
+  else if (mouseX >  375 && mouseX < 425 && mouseY > 475 && mouseY < 525){
+    pot = "four";
+    plant_seed(pot, "red");
+  }
+  else if (mouseX > 575 && mouseX <625 && mouseY > 475 && mouseY < 525){
+    pot = "five";
+    plant_seed(pot, "red");
+  }
+  else if (mouseX > 775 && mouseX < 825 && mouseY > 475 && mouseY < 525){
+    pot = "six";
+    plant_seed(pot, "red");
+  }
 }
